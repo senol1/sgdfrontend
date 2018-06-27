e66f01f00fea0ca0deb2832a1e868ea2ce692d7d
Sény Mbaye <senyr9@gmail.com>
2018-06-27 18:05:36 +0000

    add guard service

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) { }
+
+  canActivate() {
+    if (localStorage.getItem('currentUser')) {
+
+        // logged in so return true
+
+        console.log( JSON.parse(localStorage.getItem('currentUser')).username);
+
+        return true;
+    }
+
+    console.log( JSON.parse(localStorage.getItem('currentUser')).username);
+
+    // not  logged in so redirect to login page
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
